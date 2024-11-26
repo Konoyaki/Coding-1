a1403843c6b156ff8a4fac5724a7ab9724d2ca78
Konoyaki <ariessantosoplays@gmail.com>
2024-11-26 10:46:57 +0700

    Changes and create

--- CRUD-PHP/index.php
+++ CRUD-PHP/index.php
@@ -2,7 +2,7 @@
 // Include the database connection file
 require_once('dbConnection.php');
 
-// Fetch data in descending order (lastest entry first)
+// Fetch data in descending order (latest entry first)
 $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC")
 ?>
 
@@ -32,7 +32,7 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC")
             echo "<td>".$res['name']."</td>";
             echo "<td>".$res['age']."</td>";
             echo "<td>".$res['email']."</td>";
-            echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> 
+            echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> |
             <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
         }
         ?>
